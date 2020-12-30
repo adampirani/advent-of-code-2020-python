ebd6f16d862928df07a4461d6fcee8eed5be2679
Adam Pirani <adam.pirani@gmail.com>
2020-12-29 20:32:47 -0500

    Day1 had two parts, leave as day1

--- day1/setup.py
+++ day1/setup.py
@@ -1,11 +1,28 @@
 EXPENSES = open('input.txt', 'r')
-TOTAL = 2020
+expenses_array = EXPENSES.read().splitlines()
 ledger = set()
 
-for expense in EXPENSES:
-    expense_int = int(expense)
-    if ((TOTAL-expense_int) in ledger):
-        print(expense_int*(TOTAL-expense_int))
-        break
-    else:
-        ledger.add(expense_int)
+
+def findProduct(total, expenses, start=0):
+    """Get the product of the two numbers that add to the total"""
+    for idx, expense in enumerate(expenses, start):
+        expense_int = int(expense)
+        if ((total-expense_int) in ledger):
+            return (expense_int*(total-expense_int))
+        else:
+            ledger.add(expense_int)
+
+
+def findTripleProduct(total, expenses):
+    """Get the product of the three numbers that add to the total"""
+    for idx, expense in enumerate(expenses):
+
+        expense_int = int(expense)
+        remainder = total-expense_int
+        remainderProduct = findProduct(remainder, expenses, idx+1)
+
+        if (remainderProduct):
+            return remainderProduct*expense_int
+
+
+print(findTripleProduct(2020, expenses_array))
